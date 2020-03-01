f7894c86f83023bc0d1716f4660e85c7247a9f4d
PurrrPurrr <s15072@pjwstk.edu.pl>
2020-03-01 17:24:36 +0100

    commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            foreach(var a in args)
+            {
+                Console.WriteLine(a);
+            }
+            var emails = await GetEmails(args[0]);
+
+            foreach(var email in emails)
+            {
+                Console.WriteLine(email);
+            }
+        }
+
+        static async Task<IList<string>> GetEmails(string url)
+        {
+            var httpClient = new HttpClient();
+            //var response = await httpClient.GetAsync(args[0]);
+            var listOfEmails = new List<string>();
+
+            var response = await httpClient.GetAsync(url);
+
+            
+            Regex emailRegex = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*",
+            RegexOptions.IgnoreCase);
+
+            MatchCollection emailMatches = emailRegex.Matches(response.Content.ReadAsStringAsync().Result);
+
+
+            foreach (var emailMatch in emailMatches)
+            {
+                listOfEmails.Add(emailMatch.ToString());
+            }
+            return listOfEmails;
         }
     }
 }
